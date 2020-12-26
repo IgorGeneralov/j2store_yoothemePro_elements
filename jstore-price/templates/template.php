4311292cbb221af45d77ad8ecc858ee3f81246fe
Igor Generalov <75606571+IgorGeneralov@users.noreply.github.com>
2020-12-26 14:49:03 +0300

    Add files via upload

---
+++ jstore-price/templates/template.php
@@ -0,0 +1,51 @@
+<?php
+$app = JFactory::getApplication();
+
+$el = $this->el($props['price_element'], [
+
+    'class' => [
+        'uk-{price_style}',
+        'uk-heading-{price_decoration}',
+        'uk-font-{price_font_family}',
+        'uk-text-{price_color} {@!price_color: background}',
+        'uk-margin-remove {position: absolute}',
+    ],
+
+]);
+
+$el = $this->el('div', [
+	
+    'class' => [
+        'uk-card uk-card-{style}',
+        'uk-panel {@!style}',
+        'uk-card uk-card-{style} [uk-card-{size}]', 
+    ],
+    'style' => [
+        'min-height: {min_height}px',
+    ],
+    'uk-grid' => true,
+    
+]);
+?>
+
+<?= $el($props, $attrs) ?>
+
+	<div class="tm-sku">
+    	<?php if ($props['sku'] == '1') : ?>	    	
+    		<?php   $link = include "{$__dir}/template-sku.php"; ?>
+		<?php endif ?>
+    
+		<?php if ($props['brand'] == '1') : ?>
+    		<?php   $link = include "{$__dir}/template-brand.php"; ?>
+		<?php endif ?>
+	</div>
+    
+    <?php if ($props['price'] == '1') : ?>
+    	<?php   $link = include "{$__dir}/template-price.php"; ?>	    
+	<?php endif ?>
+    
+    <?php if ($props['cart'] == '1') : ?>
+		<?php   $link = include "{$__dir}/template-cart.php"; ?> 	
+	<?php endif ?>
+		
+<?= $el->end() ?>
